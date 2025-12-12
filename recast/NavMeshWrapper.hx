81a7cc496a1d5338a9dc8e0250e4ce64dd745da1
GlassySundew <glassysundew@gmail.com>
2025-12-12 21:10:54 +0400

    move buffer allocations into class space to not allocate every pathfinding call

--- recast/NavMeshWrapper.hx
+++ recast/NavMeshWrapper.hx
@@ -18,6 +18,20 @@ class NavMeshWrapper {
 	final inst : NavMesh;
 	final query : NavMeshQuery;
 
+	// buff allocations
+	final filter = new recast.Recast.QueryFilter();
+	final polys = new NativeArray<Int>( MAX_PATH );
+	final straight = new NativeArray<Single>( 3 * MAX_PATH );
+	final straightFlags = new NativeArray<hl.UI8>( MAX_PATH );
+	final straightRefs = new NativeArray<Int>( MAX_PATH );
+
+	final startPtr = new NativeArray<Single>( 3 );
+	final endPtr = new NativeArray<Single>( 3 );
+	final ext = new NativeArray<Single>( 3 );
+
+	final nearestStart = new NativeArray<Single>( 3 );
+	final nearestEnd = new NativeArray<Single>( 3 );
+
 	public function new( bytes : Bytes ) {
 
 		inst = new NavMesh();
@@ -26,22 +40,24 @@ class NavMeshWrapper {
 			inst, bytes.getData().bytes, bytes.length
 		);
 		final initSt = DetourStatus.fromInt( initResult );
+
+		#if debug
 		if ( !initSt.contains( SUCCESS ) )
 			trace( "error while nav mesh init: " + initSt );
+		#end
 
 		query = new NavMeshQuery();
 
 		final stQueryInit = query.init( inst, 2048 );
 		final queryInitSt = DetourStatus.fromInt( stQueryInit );
+
+		#if debug
 		if ( !queryInitSt.contains( SUCCESS ) )
 			trace( "error while nav mesh query init: " + queryInitSt );
+		#end
 	}
 
 	public function findPath( start : Point, end : Point ) : Array<Point> {
-
-		final startPtr = new NativeArray<Single>( 3 );
-		final endPtr = new NativeArray<Single>( 3 );
-		final ext = new NativeArray<Single>( 3 );
 
 		// recast y up
 		startPtr[0] = start.x;
@@ -56,28 +72,20 @@ class NavMeshWrapper {
 		ext[1] = EXT_Y;
 		ext[2] = EXT_Z;
 
-		final filter = new recast.Recast.QueryFilter();
-
 		var startVal = 0;
 		final startRef = new Ref<Int>( startVal );
 		var endVal = 0;
 		final endRef = new Ref<Int>( endVal );
 
-		final nearestStart = new NativeArray<Single>( 3 );
-		final nearestEnd = new NativeArray<Single>( 3 );
-
 		var st = query.findNearestPoly( startPtr, ext, filter, startRef, nearestStart );
-		trace( 'findNearestPoly(start) st=$st ref=${startRef.get()}' );
 
 		st = query.findNearestPoly( endPtr, ext, filter, endRef, nearestEnd );
-		trace( 'findNearestPoly(end) st=$st ref=${endRef.get()}' );
 
 		if ( startRef.get() == 0 || endRef.get() == 0 ) {
 
 			trace( 'No valid start or end poly' );
 		}
 
-		final polys = new NativeArray<Int>( MAX_PATH );
 		var pathCountVal = 0;
 		final pathCountRef = new Ref<Int>( pathCountVal );
 
@@ -92,13 +100,13 @@ class NavMeshWrapper {
 			MAX_PATH
 		);
 		final findPathSt = DetourStatus.fromInt( st );
+
+		#if debug
 		if ( !findPathSt.contains( SUCCESS ) )
 			trace( "error while finding path: " + findPathSt );
+		#end
 
 		final maxStraight = MAX_PATH;
-		final straight = new NativeArray<Single>( 3 * maxStraight );
-		final straightFlags = new NativeArray<hl.UI8>( maxStraight );
-		final straightRefs = new NativeArray<Int>( maxStraight );
 		var straightCountVal = 0;
 		final straightCountRef = new Ref<Int>( straightCountVal );
 		final polyCount = pathCountRef.get();
@@ -116,7 +124,6 @@ class NavMeshWrapper {
 		);
 
 		final straightCount = straightCountRef.get();
-		trace( 'findStraightPath st=${recast.DetourStatus.fromInt( st )} count=$straightCount' );
 
 		final out : Array<Point> = [];
 		out.resize( straightCount );
@@ -126,7 +133,7 @@ class NavMeshWrapper {
 			final ry = straight[i * 3 + 1];
 			final rz = straight[i * 3 + 2];
 
-			out.push( new Point( rx, -rz, ry ) );
+			out[i] = new Point( rx, -rz, ry );
 		}
 
 		return out;
